c5ac87975133a71d510973b366609b3a8417c41b
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-12 15:36:23 -0700

    Javascript Operators and Expressions | Rachel Beacham

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+
+var width = 25;
+var height = 50;
+var area = width * height;
+console.log('Value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 100;
+var payment = 200;
+var change = payment - bill;
+console.log('Value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+var quizzes = 89;
+var midterm = 90;
+var final = 95;
+var grade = (quizzes + midterm + final) / 3;
+console.log('Value of Grade: ', grade);
+console.log('type of Grade: ', typeof grade);
+
+var firstName = 'Rachel';
+var lastName = 'Beacham';
+var fullName = firstName + ' ' + lastName;
+console.log('Value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+var pH = 3;
+var isAcidic = pH < 7;
+console.log('Value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+var headCount = 86;
+var isSparta = headCount === 300;
+console.log('Value of isSparta: ', isSparta);
+console.log('type of isSparta: ', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+console.log('Value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
