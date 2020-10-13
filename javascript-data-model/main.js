2d096337080c871861d8fa92aa9c761e21e35ed7
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-13 11:43:11 -0700

    Javascript Data Model

---
+++ javascript-data-model/main.js
@@ -0,0 +1,73 @@
+/* exported orderHistory */
+var orderHistory = [
+
+  {
+    orderOne: [
+      {
+        title: 'GameCube',
+        type: 'controller',
+        edition: 'Super Smash Bros',
+        quantity: 1,
+        orderDate: 'July 3rd',
+        deliveredDate: 'July 5th',
+        returnWindow: 'Closed August 4th 2020',
+        price: 94.95,
+        orderNumber: '113-2883177-2648248',
+        shipTo: 'JS Masher'
+      },
+
+      {
+        title: 'The Art of Sql',
+        type: 'book',
+        author: 'Faroult, Stephanie',
+        quantity: 1,
+        orderDate: 'July 3rd',
+        deliveredDate: 'July 5th',
+        returnWindow: 'Closed August 4th 2020',
+        price: 33.99,
+        orderNumber: '113-2883177-2648248',
+        shipTo: 'JS Masher'
+      }
+    ]
+  },
+
+  {
+    title: 'GameCube',
+    type: 'Controller Adapter',
+    edition: 'Super Smash Bros Switch',
+    quantity: 1,
+    orderDate: 'July 4th',
+    deliveredDate: 'July 7th',
+    returnWindow: 'Closed August 5th',
+    price: 15.98,
+    orderNumber: '114-2875557-9059409',
+    shipTo: 'JS Masher'
+  },
+
+  {
+    title: 'The Timeless Way of Building',
+    type: 'book',
+    author: 'Alexander, Christopher',
+    quantity: 1,
+    orderDate: 'July 19th',
+    deliveredDate: 'July 20th',
+    returnWindow: 'Closed August 19th',
+    cost: 41.33,
+    orderNumber: '113-9984268-1280257',
+    shipTo: 'JS Masher'
+  },
+
+  {
+    title: 'JavaScript for impatient programmers',
+    type: 'book',
+    author: 'Rauschmayer, Dr. Axel',
+    quantity: 1,
+    orderDate: 'August 4th',
+    deliveredDate: 'August 8th',
+    returnWindow: 'Closed September 7th',
+    cost: 31.55,
+    orderNumber: '114-3941689-8772232',
+    shipTo: 'JS Masher'
+  }
+];
+console.log('Value for orderHistory: ', orderHistory);
