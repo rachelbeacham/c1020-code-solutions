a72256c4407c56018e0d7c9d718a253d095024b7
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-13 17:43:59 -0700

    Changes applied

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -2,72 +2,77 @@
 var orderHistory = [
 
   {
-    orderedItems: [
+    orderDate: 'July 3rd',
+    deliveredDate: 'July 5th',
+    returnWindow: 'Closed August 4th 2020',
+    orderNumber: '113-2883177-2648248',
+    shipTo: 'JS Masher',
+    items: [
       {
         title: 'GameCube',
         type: 'controller',
         createdBy: 'Nintendo',
         quantity: 1,
-        orderDate: 'July 3rd',
-        deliveredDate: 'July 5th',
-        returnWindow: 'Closed August 4th 2020',
-        price: 94.95,
-        orderNumber: '113-2883177-2648248',
-        shipTo: 'JS Masher'
+        price: 94.95
       },
-
       {
         title: 'The Art of Sql',
         type: 'book',
         createdBy: 'Faroult, Stephanie',
         quantity: 1,
-        orderDate: 'July 3rd',
-        deliveredDate: 'July 5th',
-        returnWindow: 'Closed August 4th 2020',
-        price: 33.99,
-        orderNumber: '113-2883177-2648248',
-        shipTo: 'JS Masher'
-      },
+        price: 33.99
+      }
+    ]
+  },
 
+  {
+    orderDate: 'July 4th',
+    deliveredDate: 'July 7th',
+    returnWindow: 'Closed August 5th',
+    orderNumber: '114-2875557-9059409',
+    shipTo: 'JS Masher',
+    items: [
       {
         title: 'GameCube',
         type: 'Controller Adapter',
         createdBy: 'Nintendo',
         quantity: 1,
-        orderDate: 'July 4th',
-        deliveredDate: 'July 7th',
-        returnWindow: 'Closed August 5th',
-        price: 15.98,
-        orderNumber: '114-2875557-9059409',
-        shipTo: 'JS Masher'
-      },
+        price: 15.98
+      }
+    ]
+  },
 
+  {
+    orderDate: 'July 19th',
+    deliveredDate: 'July 20th',
+    returnWindow: 'Closed August 19th',
+    orderNumber: '113-9984268-1280257',
+    shipTo: 'JS Masher',
+    items: [
       {
         title: 'The Timeless Way of Building',
         type: 'book',
         createdBy: 'Alexander, Christopher',
         quantity: 1,
-        orderDate: 'July 19th',
-        deliveredDate: 'July 20th',
-        returnWindow: 'Closed August 19th',
-        price: 41.33,
-        orderNumber: '113-9984268-1280257',
-        shipTo: 'JS Masher'
-      },
+        price: 41.33
+      }
+    ]
+  },
 
+  {
+    orderDate: 'August 4th',
+    deliveredDate: 'August 8th',
+    returnWindow: 'Closed September 7th',
+    orderNumber: '114-3941689-8772232',
+    shipTo: 'JS Masher',
+    items: [
       {
         title: 'JavaScript for impatient programmers',
         type: 'book',
         createdBy: 'Rauschmayer, Dr. Axel',
         quantity: 1,
-        orderDate: 'August 4th',
-        deliveredDate: 'August 8th',
-        returnWindow: 'Closed September 7th',
-        price: 31.55,
-        orderNumber: '114-3941689-8772232',
-        shipTo: 'JS Masher'
+        price: 31.55
       }
     ]
   }
 ];
-console.log('Value for orderHistory: ', orderHistory);
