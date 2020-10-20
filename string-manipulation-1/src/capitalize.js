ead4097b5b81a3274d31c05e7d25e77e7d36dcd5
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-19 17:42:48 -0700

    not done

--- string-manipulation-1/src/capitalize.js
+++ string-manipulation-1/src/capitalize.js
@@ -1,10 +1,4 @@
 /* eslint-disable no-unused-vars */
 function capitalize(word) {
-  for (var i = 0; i < word.length; i++) {
-    if (i === 0) {
-      word.toUppercase(word[i]);
-    }
-  }
-  word.toLowerCase(word[i]);
-  return word;
+  return word[0].toUpperCase() + word.toLowerCase();
 }
