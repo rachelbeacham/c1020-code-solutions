70abdd0537625a751c9354f2a846b265fa44cf4c
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-20 17:50:14 -0700

    Changes applied

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,11 +1,13 @@
 var $openButton = document.querySelector('.open');
-var $surveyQuestion = document.querySelector('.modal');
+var $surveyQuestion = document.querySelector('.hidden');
 var $noButton = document.querySelector('.survey');
+var $box = document.querySelector('.box');
 
 $openButton.addEventListener('click', function () {
   $surveyQuestion.className = 'active';
+  $box.className = 'box modal';
 });
 $noButton.addEventListener('click', function () {
-  $surveyQuestion.className = 'modal';
-
+  $surveyQuestion.className = 'hidden';
+  $box.className = 'box';
 });
