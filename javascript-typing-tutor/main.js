5d2acbca77e52c665e859b4938d8da5aaecba026
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-22 20:30:11 -0700

    Javascript typing tutor | Rachel Beacham

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,11 +1,25 @@
-var letter = document.querySelectorAll('span');
-var body = document.querySelector('body');
+var $letters = document.querySelectorAll('span');
+var $body = document.querySelector('body');
+var $button = document.querySelector('button');
+var $h1 = document.querySelector('h1');
+console.log($letters);
+var index = 0;
+var currentChar = $letters[index];
 
-body.addEventListener('keydown', function (event) {
+$body.addEventListener('keydown', function (event) {
   var charEntered = event.key;
   console.log(charEntered);
-
-  if (charEntered === letter) {
-    letter.className = 'correct';
+  if (index === $letters.length - 1) {
+    $button.className = 'shown';
+    $h1.className = 'hidden';
+    return;
+  }
+  if (charEntered === currentChar.textContent) {
+    currentChar.className = 'correct';
+    index++;
+    currentChar = $letters[index];
+    currentChar.className = 'next';
+  } else {
+    currentChar.className = 'wrong next';
   }
 });
