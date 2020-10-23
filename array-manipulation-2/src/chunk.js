f8dd3da5fdbcdae47c35c680772808e34c46f2c5
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-23 10:04:47 -0700

    not done

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function chunk(array, size) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    var section = array.slice(0, size);
+    var section2 = array.slice(-size);
+
+  }
+  newArray.push(section);
+  newArray.push(section2);
+  return newArray;
+}
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,5 @@
 /* eslint-disable no-unused-vars */
+function dropRight(array, count) {
+  var newArray = array.slice(0, -count);
+  return newArray;
+}
