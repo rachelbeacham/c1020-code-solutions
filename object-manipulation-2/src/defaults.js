569abf4c63ff739b7011056193c95afcbd1f4b83
Rachel Beacham <rachelbeacham17@gmail.com>
2020-11-02 09:48:15 -0800

    not done

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,7 @@
 /* eslint-disable no-unused-vars */
+function defaults(target, source) {
+  for (var property in source) {
+    target[property] = source[property];
+  }
+  console.log(target);
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -2,15 +2,12 @@
 function omit(source, keys) {
   debugger;
   var newObj = {};
-  for (var key in source) {
-    for (var i = 0; i < keys.length; i++) {
-      if (keys[i] === key) {
-        break;
-      } else if (keys[i] !== key) {
+  for (var i = 0; i < keys.length; i++) {
+    for (var key in source) {
+      if (keys[i] !== key) {
         newObj[key] = source[key];
-
       }
     }
-  } return newObj;
-
+  }
+  return newObj;
 }
