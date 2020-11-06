f120b69e984312a04e7fb9a297e014bef6170cec
Rachel Beacham <rachelbeacham17@gmail.com>
2020-11-05 22:05:17 -0800

    3 done on 4

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,16 @@
 /* eslint-disable no-unused-vars */
+function capitalizeWords(string) {
+  debugger;
+  var firstChar = string[0].toUpperCase();
+  var lowerChar = '';
+  for (var i = 1; i < string.length; i++) {
+    if (string[i] === ' ') {
+      var newWordChar = string[i + 1].toUpperCase();
+    } else {
+      lowerChar += string[i].toLowerCase();
+    }
+
+  }
+  string = firstChar + lowerChar + newWordChar + lowerChar;
+  return string;
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
 function ransomCase(string) {
-  debugger;
-  for (var i = 0; i < string.length; i += 2) {
-    string = string[i].toLowerCase() + string[i + 1].toUpperCase();
+  var newString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 === 1) {
+      newString += string[i].toUpperCase();
+    } else {
+      newString += string[i].toLowerCase();
+    }
   }
-  return string;
+  return newString;
 }
