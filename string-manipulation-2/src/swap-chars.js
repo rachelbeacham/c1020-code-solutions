85a886048affbf1f2e7ccb4f6ebeb5206c7bf103
Rachel Beacham <rachelbeacham17@gmail.com>
2020-11-08 22:53:09 +0000

    String Manipulation 2 | Rachel Beacham

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,4 +1,14 @@
 /* eslint-disable no-unused-vars */
 function swapChars(firstIndex, secondIndex, string) {
-
+  var phrase = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      phrase += string[secondIndex];
+    } else if (i === secondIndex) {
+      phrase += string[firstIndex];
+    } else {
+      phrase += string[i];
+    }
+  }
+  return phrase;
 }
