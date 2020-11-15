cf2ae530d9c1e58204b9207e4f5775eccf53b418
Rachel Beacham <rachelbeacham17@gmail.com>
2020-11-15 17:02:34 +0000

    String Manipulation 3 Completed | Rachel Beacham

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,10 +2,11 @@
 function titleCase(title) {
   var titleArray = title.split(' ');
   var newArray = [];
+  var newIndividuals = [];
   for (var i = 0; i < titleArray.length; i++) {
     var current = titleArray[i];
     if (current.length >= 4) {
-      if (current === 'javascript' || current === 'javascript:') {
+      if (current === 'javascript' || current === 'Javascript:' || current === 'javascript:') {
         var j = current[0].toUpperCase();
         var middle = current.substring(1, 4).toLowerCase();
         var s = current[4].toUpperCase();
@@ -29,5 +30,21 @@ function titleCase(title) {
     }
   }
   var newString = newArray.join(' ');
-  return newString;
+  var individuals = newString.split('');
+  for (var x = 0; x < individuals.length; x++) {
+    if (individuals[x] === ':') {
+      var upperCased = individuals[x + 2].toUpperCase();
+      newIndividuals.push(individuals[x] + ' ' + upperCased);
+      x += 3;
+    }
+    if (individuals[x] === '-') {
+      var capital = individuals[x + 1].toUpperCase();
+      newIndividuals.push(individuals[x] + capital);
+      x += 1;
+    } else {
+      newIndividuals.push(individuals[x]);
+    }
+  }
+  var finalString = newIndividuals.join('');
+  return finalString;
 }
