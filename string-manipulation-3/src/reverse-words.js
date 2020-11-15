fbe1984dc789a64c073d8cb7feaf155d4684a2fc
Rachel Beacham <rachelbeacham17@gmail.com>
2020-11-15 04:27:22 +0000

    3 done, on 4

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+function reverseWords(string) {
+  var stringArray = string.split(' ');
+  var reverseString = '';
+  for (var i = 0; i < stringArray.length; i++) {
+    var current = stringArray[i];
+    for (var j = current.length - 1; j >= 0; j--) {
+      reverseString += current[j];
+      if (i !== stringArray.length - 1) {
+        if (j === 0) {
+          reverseString += ' ';
+        }
+      }
+    }
+  }
+  return reverseString;
+}
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,33 @@
 /* eslint-disable no-unused-vars */
+function titleCase(title) {
+  var titleArray = title.split(' ');
+  var newArray = [];
+  for (var i = 0; i < titleArray.length; i++) {
+    var current = titleArray[i];
+    if (current.length >= 4) {
+      if (current === 'javascript' || current === 'javascript:') {
+        var j = current[0].toUpperCase();
+        var middle = current.substring(1, 4).toLowerCase();
+        var s = current[4].toUpperCase();
+        var end = current.substring(5).toLowerCase();
+        current = j + middle + s + end;
+        newArray.push(current);
+      } else {
+        var firstLetter = current[0].toUpperCase();
+        var rest = current.substring(1);
+        var word = firstLetter + rest;
+        newArray.push(word);
+      }
+    } else if (current === 'api') {
+      current = current.toUpperCase();
+      newArray.push(current);
+    } else if (current === 'web') {
+      current = current[0].toUpperCase() + current[1] + current[2];
+      newArray.push(current);
+    } else {
+      newArray.push(current);
+    }
+  }
+  var newString = newArray.join(' ');
+  return newString;
+}
