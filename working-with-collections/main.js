12b6a6dde1a47a27496b44491b48f5872e3f1fd4
Rachel Beacham <rachelbeacham17@gmail.com>
2020-11-20 00:59:07 +0000

    started working-with-collections

---
+++ working-with-collections/main.js
@@ -0,0 +1,21 @@
+/* global _ */
+/* eslint-disable no-console */
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+var players = [
+  {
+    name: 'Rachel',
+    hand: null
+  },
+  {
+    name: 'Stephen',
+    hand: null
+  },
+  {
+    name: 'Ruby',
+    hand: null
+  },
+  {
+    name: 'Kris',
+    hand: null
+  }
+];
