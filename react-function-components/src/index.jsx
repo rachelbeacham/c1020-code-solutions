2c51db47bf8e135850abcfc94dbc7951331f61aa
Rachel Beacham <rachelbeacham17@gmail.com>
2020-12-09 18:52:44 +0000

    Completed React-Function-Components

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomeButton() {
+  return <button>Click Me!</button>;
+}
+ReactDOM.render(
+  CustomeButton(),
+  document.getElementById('root')
+);
